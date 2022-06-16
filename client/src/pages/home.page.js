96e1b2431d3f09a13c91f633dba30d731e8e6482
vignesh <vignesh@vigneshs-MacBook-Air.local>
2022-06-16 23:45:32 +0530

    fix(app): removing message context on logout

--- client/src/pages/home.page.js
+++ client/src/pages/home.page.js
@@ -1,84 +1,100 @@
-import { Row, Col, Button } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
-import {useSubscription } from "@apollo/client";
-import {MESSAGE_CREATED_SUBSCRIPTION,NEW_REACTION_SUBSCRIPTION } from "../gql/queries"
-import { useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useSubscription } from "@apollo/client";
+import {
+  MESSAGE_CREATED_SUBSCRIPTION,
+  NEW_REACTION_SUBSCRIPTION,
+} from "../gql/queries";
+import { useEffect,useState } from "react";
 import Users from "../components/home/users";
-import { useAuthDispatch ,useAuthState} from "../utils/auth.util";
+import { useAuthDispatch, useAuthState } from "../utils/auth.util";
 import { useApolloClient } from "@apollo/client";
 import Messages from "../components/home/messages";
 import { useMessageDispatch } from "../utils/messages.utils";
-import { DropdownButton ,Dropdown} from "react-bootstrap";
+import { DropdownButton, Dropdown } from "react-bootstrap";
+import classNames from "classnames";
 const Home = () => {
   const navigate = useNavigate();
   const authDispatch = useAuthDispatch();
-  const messageDispatch=useMessageDispatch();
-  const {user}=useAuthState()
+  const messageDispatch = useMessageDispatch();
+  const { user } = useAuthState();
   const client = useApolloClient();
+  const [expandMenu,setExpandMenu]=useState(false)
 
-  const {data:messageData,error:messageError}=useSubscription(MESSAGE_CREATED_SUBSCRIPTION,{
-    onSubscriptionComplete(){
-      console.log("hi")
+  const toggleMenu=()=>{
+    setExpandMenu(!expandMenu)
+  }
+
+  const { data: messageData, error: messageError } = useSubscription(
+    MESSAGE_CREATED_SUBSCRIPTION,
+    {
+      onSubscriptionComplete() {
+        console.log("hi");
+      },
     }
-  })
-  const {data:reactionData,error:reactionError}=useSubscription(NEW_REACTION_SUBSCRIPTION)
-
+  );
+  const { data: reactionData, error: reactionError } = useSubscription(
+    NEW_REACTION_SUBSCRIPTION
+  );
 
   const logout = () => {
     messageDispatch({
-      type:"LOGOUT",
-      payload:{
-username:user.username
-
-      }
-    })
+      type: "LOGOUT",
+      payload: {
+        username: user.username,
+      },
+    });
     authDispatch({
       type: "LOGOUT",
     });
- 
+
     client.resetStore();
     navigate("/login");
   };
 
-  useEffect(()=>{
-    console.log(messageData,messageError)
-    if(messageError){
+  useEffect(() => {
+    console.log(messageData, messageError);
+    if (messageError) {
       // window.location.href="./"
     }
-    if(messageData){
-      const message=messageData.messageCreated
-      const otherUser=user.username===message.to?message.from:message.to
-      console.log("new message")
+    if (messageData) {
+      const message = messageData.messageCreated;
+      const otherUser =
+        user.username === message.to ? message.from : message.to;
+      console.log("new message");
       messageDispatch({
-        type:"ADD_MESSAGE",
-        payload:{
-          username:otherUser, 
-          message
-        }
-    })
-  }
-},[messageData,messageError])
+        type: "ADD_MESSAGE",
+        payload: {
+          username: otherUser,
+          message,
+        },
+      });
+    }
+  }, [messageData, messageError]);
 
-useEffect(()=>{
-  console.log(reactionData,reactionError)
-  if(reactionError){
-    // window.location.href="./"
-    console.log(reactionError)
-  }
-  if(reactionData){
-    const reaction=reactionData.newReaction
-    console.log(reactionData);
-    const otherUser=user.username===reaction.message.to?reaction.message.from:reaction.message.to
-    console.log(otherUser);
-    messageDispatch({
-      type:"ADD_REACTION",
-      payload:{
-        username:otherUser, 
-        reaction
-      }
-  })
-}
-},[reactionData,reactionError])
+  useEffect(() => {
+    console.log(reactionData, reactionError);
+    if (reactionError) {
+      // window.location.href="./"
+      console.log(reactionError);
+    }
+    if (reactionData) {
+      const reaction = reactionData.newReaction;
+      console.log(reactionData);
+      const otherUser =
+        user.username === reaction.message.to
+          ? reaction.message.from
+          : reaction.message.to;
+      console.log(otherUser);
+      messageDispatch({
+        type: "ADD_REACTION",
+        payload: {
+          username: otherUser,
+          reaction,
+        },
+      });
+    }
+  }, [reactionData, reactionError]);
 
   return (
     <div>
@@ -89,12 +105,14 @@ useEffect(()=>{
           </Link>
         </Col> */}
 
-        <Col style={{textAlign:"right"}}>
+        <Col style={{ textAlign: "right" }}>
           {/* <Link style={{ textDecoration: "none" }} to="/login">
             Login
           </Link> */}
-          <DropdownButton className={"px-3"}title={user.username}>
-          <Dropdown.Item  eventKey="1" onClick={logout}>Logout</Dropdown.Item>
+          <DropdownButton className={"px-3"} title={user.username}>
+            <Dropdown.Item eventKey="1" onClick={logout}>
+              Logout
+            </Dropdown.Item>
           </DropdownButton>
         </Col>
 
@@ -105,8 +123,14 @@ useEffect(()=>{
         </Col> */}
       </Row>
       <Row className="bg-dark pr-3 pt-2">
-          <Users />
+        <Col xs={expandMenu? 10:2} md={4} style={{ padding: 0 }}>
+          <Users expandMenu={expandMenu} toggleMenu={toggleMenu} />
+        </Col>
+        <Col xs={10} md={8} className={classNames("",{
+          "d-none":expandMenu
+        })}>
           <Messages />
+        </Col>
       </Row>
     </div>
   );
