d5881b656ba6d366cd3a19c21519686cb368e9bf
vignesh <vignesh@vigneshs-MacBook-Air.local>
2022-06-16 23:46:37 +0530

    fix(app): close other reaction panels when one is opened and long message

--- client/src/components/home/message.js
+++ client/src/components/home/message.js
@@ -1,22 +1,23 @@
 import { useAuthState } from "../../utils/auth.util";
 import classNames from "classnames";
 import moment from "moment";
-import { OverlayTrigger, Tooltip, Button, Popover } from "react-bootstrap";
-import { REACT_TO_MESSAGE } from "../../gql/queries";
+import { OverlayTrigger, Tooltip, Button, Popover ,Toast} from "react-bootstrap";
+import { REACT_TO_MESSAGE , SET_AS_READ_MUTATION} from "../../gql/queries";
 import { useMutation } from "@apollo/client";
-import { BsEmojiHeartEyes } from "react-icons/bs";
-import { useState } from "react";
+import { BsEmojiHeartEyes, } from "react-icons/bs";
+import { useEffect,  } from "react";
 
-const Message = ({ message }) => {
+const Message = ({ message,reactionOpen,setReactionOpen}) => {
   const { user } = useAuthState();
   const sent = message.from === user.username;
   const recieved = !sent;
-  const [showReactions, setShowReactions] = useState(false);
   const reactions = ["❤️", "😆", "😯", "😢", "😡", "👍", "👎"];
+  const messageLenght=message.content.length>59
   const [reactToMessage] = useMutation(REACT_TO_MESSAGE,{
     onError:(err)=>console.log(err),
-    onCompleted:()=>setShowReactions(false)
+    onCompleted:()=>setReactionOpen('')
   });
+  
   // const reactionIcons=[...new Set(message.message_reactions.map(r=>r.content))]
 
   const onReact = (reaction) => {
@@ -27,6 +28,19 @@ const Message = ({ message }) => {
       },
     });
   };
+  const [setRead]=useMutation(SET_AS_READ_MUTATION)
+  useEffect(()=>{
+    console.log(message.read)
+    if(message.read===false && message.from!==user.username){
+        setRead({
+          variables:{
+            setAsReadId:message._id
+          }
+        }
+        )
+    }
+
+  },[message])
   const PopoverContent = (
     <Popover id="popover-basic" className="rounded-pill">
       <Popover.Body className="d-flex align-items-center reaction-icons-popover">
@@ -42,13 +56,14 @@ const Message = ({ message }) => {
       </Popover.Body>
     </Popover>
   );
+ 
   const ReactionButton = (
     <OverlayTrigger
       placement="top"
       trigger="click"
       overlay={PopoverContent}
-      show={showReactions}
-      onToggle={() => setShowReactions(!showReactions)}
+      show={reactionOpen===message._id }
+      onToggle={() => setReactionOpen(message._id)}
     >
       <Button variant="link" className="px-2 reaction-icons">
         <BsEmojiHeartEyes size="1.5rem" />
@@ -57,9 +72,10 @@ const Message = ({ message }) => {
   );
   return (
     <div
-      className={classNames("d-flex my-3", {
+      className={classNames("d-flex my-1", {
         "sent-message": sent,
         "revieved-message": recieved,
+        "my-4":message.message_reactions.length> 0
       })}
     >
       {sent && ReactionButton}
@@ -73,9 +89,10 @@ const Message = ({ message }) => {
         }
       >
         <div
-          className={classNames("py-2 px-3 mt-2 rounded-pill position-relative", {
+          className={classNames("py-2 px-3 mt-1 rounded-pill position-relative", {
             "bg-primary": sent,
-            "bg-secondary": recieved,
+            "bg-secondary text-primary": recieved,
+            "long-message-border":messageLenght
           })}
         >
           {message.message_reactions.length> 0 && (
