e74d599aa08985b8650ca399e8212a86549e9f4a
vignesh <vignesh@vigneshs-MacBook-Air.local>
2022-06-23 00:13:22 +0530

    fix(client): toast message close and positioning

--- client/src/components/general/toastMessage.js
+++ client/src/components/general/toastMessage.js
@@ -1,21 +1,21 @@
-
-import { useState } from "react"
-import {Toast} from "react-bootstrap"
-const ToastMessage=({messageHeader,messageBody,show,onClose})=>{
-  function closeToast(e){
-    e.preventDefault()
-    onClose(false)
+import { Toast } from "react-bootstrap";
+const ToastMessage = ({ messageHeader, messageBody, show, onClose }) => {
+  function closeToast(e) {
+    e.preventDefault();
+    onClose(false);
   }
-  console.log(show)
-    return (
-        <Toast show={show} onClose={closeToast}style={{position:"absolute",top:30,right:20,zIndex:1,}}>
-        <Toast.Header  >
-          <strong className="me-auto text-dark">{messageHeader}</strong>
-        </Toast.Header>
-        <Toast.Body className="text-white bg-dark">{messageBody}</Toast.Body>
-      </Toast>
-    )
+  return (
+    <Toast
+      show={show}
+      onClose={closeToast}
+      style={{ position: "absolute", top: 30, right: 20, zIndex: 1 }}
+    >
+      <Toast.Header>
+        <strong className="me-auto text-dark">{messageHeader}</strong>
+      </Toast.Header>
+      <Toast.Body className="text-white bg-dark">{messageBody}</Toast.Body>
+    </Toast>
+  );
+};
 
-}
-
-export default ToastMessage
+export default ToastMessage;
